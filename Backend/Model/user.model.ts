5015f88b8bedd3b6d1b70b3cf363254a306eeaf5
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-11 20:44:12 +0500

    user model created

---
+++ Backend/Model/user.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IUser extends Document {
+    name:string,
+    email:string,
+    password:string,
+    avatar:{
+        public_id:string,
+        url:string
+    },
+    role:string,
+    isVerified:boolean,
+    courses:string[]
+}
+
+const userSchema:Schema<IUser> = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'User name is required']
+    },
+    email:{
+        type:String,
+        required:[true,'User email is required'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    avatar:{
+        public_id : String,
+        url:String
+    },
+    role:{
+        type:String,
+        default:'user'
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    courses:[String]
+
+})
+
+export const userModel:Model<IUser> = mongoose.model<IUser>('User',userSchema)
