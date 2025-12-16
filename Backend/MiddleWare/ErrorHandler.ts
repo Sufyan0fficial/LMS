37958a2ffe34364e68e37b8e0c44394ade12c438
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-16 21:05:07 +0500

    created Error handling middle ware

--- Backend/MiddleWare/ErrorHandler.ts
+++ Backend/MiddleWare/ErrorHandler.ts
@@ -1,8 +1,6 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 export const ErrorHandler:ErrorRequestHandler = (err, req, res, next)=>{
-    //here type ErrorRequestHandler is considered as 
-    // type ErrorRequestHandler = (err:any, req:Request, res:Response, next:NextFunction)=>void
     const message = err.message || 'Internal Server Error'
     const statusCode = err.statusCode || 500
     return res.status(statusCode).json({
