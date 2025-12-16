10d70c904d63828a1d7d3baa5be283b0523a61d3
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-16 21:10:28 +0500

    creating access and refresh token and updating user data to redis

---
+++ Backend/Utils/jwt.ts
@@ -0,0 +1,44 @@
+import { CookieOptions, Response } from "express";
+import { IUser } from "../Model/user.model.js";
+import jwt, { Secret } from 'jsonwebtoken'
+import { redisClient } from "../Redis/init.redis.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+
+export const SendCookie = (user:IUser,statusCode:number,res:Response, flag?:boolean)=>{
+    const access_Token = jwt.sign({id:user?._id},process.env.ACCESS_TOKEN as string , {expiresIn:'10m'})
+    const refresh_Token = jwt.sign({id:user?._id},process.env.REFRESH_TOKEN as string, {expiresIn:'7d'})
+
+    const accessTokenExpires = parseInt(process.env.ACCESS_TOKEN_EXPIRES as string ,10) * 60 * 60 *  1000
+    const refreshTokenExpires = parseInt(process.env.REFRESH_TOKEN_EXPIRES as string ,10) * 24 * 60 * 60 * 1000
+
+    if(!flag){
+        redisClient.set(`${user?._id}`, JSON.stringify(user)) 
+    }
+    
+
+    const accessTokenOptions:CookieOptions = {
+        maxAge : accessTokenExpires,
+        httpOnly : true,
+        secure : process.env.NODE_ENV === 'production',
+        sameSite:'lax'
+    }
+    const refreshTokenOptions:CookieOptions = {
+        maxAge : refreshTokenExpires,
+        httpOnly : true,
+        secure : process.env.NODE_ENV === 'production',
+        sameSite:'lax'
+    }
+
+    res.cookie('access_token',access_Token,accessTokenOptions)
+    res.cookie('refresh_token',refresh_Token,refreshTokenOptions)
+    return res.status(statusCode).json({
+        success:true,
+        data:user,
+        accessToken:access_Token
+
+    })
+
+}
