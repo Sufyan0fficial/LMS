97555baf997dc965fbdbbeec66f4fc859e22d74a
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-16 21:11:02 +0500

    custom error to be thrown to Error handling middleware

--- Backend/Utils/customError.ts
+++ Backend/Utils/customError.ts
@@ -1,4 +1,4 @@
-const customError = (statusCode:number, message:string)=>{
+export const customError = (statusCode:number, message:string)=>{
     const error:any = new Error()
     error.message = message || 'Soemthing went wrong'
     error.statusCode = statusCode || 500
