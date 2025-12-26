e60bc1d5c4178fd27f68803104cafb9c82f05fca
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 21:01:05 +0500

    added order routes

---
+++ Backend/Routes/order.route.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { Create_Order, Get_All_Orders } from '../Controller/order.controller.js'
+import { Authorize_Role, Verify_User } from '../MiddleWare/auth.js'
+const router = express.Router()
+
+router.route('/create').post(Verify_User,Create_Order)
+router.route('/get-all-orders').get(Verify_User,Authorize_Role('admin'),Get_All_Orders)
+
+
+export default router
