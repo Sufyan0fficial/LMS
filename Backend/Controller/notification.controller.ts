cf2abc9b0d8eb563bca1c3ad76221bd602977467
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 20:57:17 +0500

    created notification controller and also used node cron to delete 30 days old read messages

---
+++ Backend/Controller/notification.controller.ts
@@ -0,0 +1,52 @@
+import { AsyncWrapper } from "../MiddleWare/AsyncWrapper.js";
+import { NotificationModel } from "../Model/notification.model.js";
+import { customError } from "../Utils/customError.js";
+import cron from "node-cron";
+
+export const Get_Notifications = AsyncWrapper(async (req, res, next) => {
+  const user = req.user;
+  const notifications = await NotificationModel.find({
+    userId: user?._id,
+  }).sort({ createdAt: -1 });
+  return res.status(200).json({
+    success: true,
+    data: notifications,
+  });
+});
+
+export const Update_Notification = AsyncWrapper(async (req, res, next) => {
+  const user = req?.user;
+  const notificationId = req.params?.id;
+  const updatedNotification = await NotificationModel.findByIdAndUpdate(
+    notificationId,
+    { status: "read" },
+    { new: true, runValidators: true }
+  );
+  if (!updatedNotification?._id) {
+    return next(customError(400, "Failed to updated Notification Status"));
+  }
+  const notifications = await NotificationModel.find({
+    userId: user?._id,
+  }).sort({ createdAt: -1 });
+  return res.status(201).json({
+    success: true,
+    data: notifications,
+  });
+});
+
+cron.schedule("0 0 * * *", async () => {
+  try {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    console.log('thirty days ago',thirtyDaysAgo)
+    await NotificationModel.deleteMany({
+      status: "read",
+      createdAt: { $lt: thirtyDaysAgo }, 
+    });
+    console.log("hi");
+  } catch (error) {
+    console.log('error : ',error)
+  }
+});
+
+
+
