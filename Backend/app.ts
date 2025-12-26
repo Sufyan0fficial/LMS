42db90775dfbe03f3ab12a11f3f81234b4c139db
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 20:42:17 +0500

    added Base routes of multiple controllers

--- Backend/app.ts
+++ Backend/app.ts
@@ -4,6 +4,12 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { ErrorHandler } from './MiddleWare/ErrorHandler.js'
 import UserAuthRouter from './Routes/userAuth.route.js'
+import CoursesRouter from './Routes/course.route.js'
+import OrderRouter from './Routes/order.route.js'
+import NotificationRouter from './Routes/notification.route.js'
+import AnalyticsRouter from './Routes/analytics.route.js'
+import LayoutRouter from './Routes/layout.route.js'
+
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
@@ -17,7 +23,10 @@ app.use(express.json({limit:'50mb'}))
 
 
 app.use('/api/v1/userAuth',UserAuthRouter)
-
+app.use('/api/v1/courses',CoursesRouter)
+app.use('/api/v1/order',OrderRouter)
+app.use('/api/v1/notification',NotificationRouter)
+app.use('/api/v1/analytics',AnalyticsRouter)
+app.use('/api/v1/layout',LayoutRouter)
 
 app.use(ErrorHandler)
-
