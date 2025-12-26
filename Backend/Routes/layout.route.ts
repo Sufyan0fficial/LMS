4d425acac0085e4712358108342853816e3a30a8
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 21:00:23 +0500

    added layout routes

---
+++ Backend/Routes/layout.route.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { Authorize_Role, Verify_User } from '../MiddleWare/auth.js'
+import { Create_Layout, Get_LayoutBy_Type, Update_Layout } from '../Controller/layout.controller.js'
+const router = express.Router()
+
+router.route('/create').post(Verify_User,Authorize_Role('admin'),Create_Layout)
+router.route('/update').patch(Verify_User,Authorize_Role('admin'),Update_Layout)
+router.route('/get-layout').post(Verify_User,Authorize_Role('admin'),Get_LayoutBy_Type)
+
+export default router
