14d4be04e9308b2bc70310142bab48816ff38426
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 21:00:01 +0500

    added course routes

---
+++ Backend/Routes/course.route.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { Authorize_Role, Verify_User } from '../MiddleWare/auth.js'
+import { Access_Course_Content, Add_Review, Answer_Question, Ask_Question, Delete_Course, Edit_Course, Get_All_Courses, Get_Courses, Get_Single_Course, Reply_Review, Upload_Course } from '../Controller/course.controller.js'
+const router = express.Router()
+
+
+router.route('/upload').post(Verify_User,Authorize_Role('admin'),Upload_Course)
+router.route('/edit/:id').post(Verify_User,Authorize_Role('admin'),Edit_Course)
+router.route('/get-course/:id').get(Get_Single_Course)
+router.route('/get-courses').get(Get_Courses)
+router.route('/get-course-content/:id').get(Verify_User,Access_Course_Content)
+router.route('/ask-question').patch(Verify_User,Ask_Question)
+router.route('/answer-question').patch(Verify_User,Answer_Question)
+router.route('/add-review/:id').patch(Verify_User,Add_Review)
+router.route('/reply-review').patch(Verify_User,Authorize_Role('admin'),Reply_Review)
+router.route('/get-all-courses').get(Verify_User,Authorize_Role('admin'),Get_All_Courses)
+router.route('/delete-course/:id').delete(Verify_User,Authorize_Role('admin'),Delete_Course)
+
+
+export default router
