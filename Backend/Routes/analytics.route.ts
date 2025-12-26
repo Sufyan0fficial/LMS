a700a9403196786e3fc48eee44b73a51f60990bb
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2025-12-26 20:59:41 +0500

    added routes for anayltics

---
+++ Backend/Routes/analytics.route.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { Authorize_Role, Verify_User } from '../MiddleWare/auth.js'
+import { Course_Analytics, Order_Analytics, User_Analytics } from '../Controller/analytics.controller.js'
+const router = express.Router()
+
+router.route('/user').get(Verify_User,Authorize_Role('admin'),User_Analytics)
+router.route('/course').get(Verify_User,Authorize_Role('admin'),Course_Analytics)
+router.route('/order').get(Verify_User,Authorize_Role('admin'),Order_Analytics)
+
+export default router
