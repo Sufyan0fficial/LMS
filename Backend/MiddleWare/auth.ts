3e91ec1208c3973f1117b9269bb442c8b6562719
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2026-01-31 02:07:51 +0500

    implemnted redis caching on login and Verify user middleware

--- Backend/MiddleWare/auth.ts
+++ Backend/MiddleWare/auth.ts
@@ -1,14 +1,13 @@
 import { customError } from "../Utils/customError.js";
 import { AsyncWrapper } from "./AsyncWrapper.js";
 import jwt from "jsonwebtoken";
-import { redisClient } from "../Redis/init.redis.js";
 import { NextFunction } from "express";
 import { Request, Response } from "express";
 import { userModel } from "../Model/user.model.js";
+import { getCache, setCache } from "../Utils/redis.cache.js";
 
 export const Verify_User = AsyncWrapper(async (req, res, next) => {
   const cookie = req.cookies?.access_token;
-  console.log("access token is", cookie);
   if (!cookie) {
     return next(customError(401, "Cookie Expired !"));
   }
@@ -18,12 +17,15 @@ export const Verify_User = AsyncWrapper(async (req, res, next) => {
   if (!user?.id) {
     return next(customError(401, "Session Expired !"));
   }
-  // const redisUser = await redisClient.get(user?.id)
-  // if(!redisUser){
-  //     return next(customError(400,'User not found'))
-  // }
-  // const userData = JSON.parse(redisUser)
-  const userData = await userModel.findById(user?.id);
+  
+  let userData = await getCache(`user:${user.id}`);
+  if (!userData) {
+    userData = await userModel.findById(user.id);
+    if (userData) {
+      await setCache(`user:${user.id}`, userData);
+    }
+  }
+  
   if (!userData?._id) {
     return next(customError(401, "User does not exist !"));
   }
--- Backend/Utils/jwt.ts
+++ Backend/Utils/jwt.ts
@@ -1,13 +1,11 @@
 import { CookieOptions, Response } from "express";
 import { IUser } from "../Model/user.model.js";
 import jwt, { Secret } from 'jsonwebtoken'
-import { redisClient } from "../Redis/init.redis.js";
+import { setCache } from "../Utils/redis.cache.js";
 import dotenv from 'dotenv'
 dotenv.config()
 
-
-
-export const SendCookie = (user:IUser,statusCode:number,res:Response, flag?:boolean)=>{
+export const SendCookie = async (user:IUser,statusCode:number,res:Response, flag?:boolean)=>{
     const access_Token = jwt.sign({id:user?._id},process.env.ACCESS_TOKEN as string , {expiresIn:'10m'})
     const refresh_Token = jwt.sign({id:user?._id},process.env.REFRESH_TOKEN as string, {expiresIn:'7d'})
 
@@ -15,21 +13,16 @@ export const SendCookie = (user:IUser,statusCode:number,res:Response, flag?:bool
     const refreshTokenExpires = parseInt(process.env.REFRESH_TOKEN_EXPIRES as string ,10) * 24 * 60 * 60 * 1000
 
     if(!flag){
-        // redisClient.set(`${user?._id}`, JSON.stringify(user)) 
+        await setCache(`user:${user._id}`, user)
     }
-    
 
     const accessTokenOptions:CookieOptions = {
         maxAge : accessTokenExpires,
         httpOnly : true,
-        // secure : process.env.NODE_ENV === 'production',
-        // sameSite:'lax'
     }
     const refreshTokenOptions:CookieOptions = {
         maxAge : refreshTokenExpires,
         httpOnly : true,
-        // secure : process.env.NODE_ENV === 'production',
-        // sameSite:'lax'
     }
 
     res.cookie('access_token',access_Token,accessTokenOptions)
