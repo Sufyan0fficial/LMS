63f0562a883c17c804d74f9165cbdb0b2ba8d50c
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2026-02-05 00:51:25 +0500

    update resend email configurations

--- Backend/Utils/sendActivationEmail.ts
+++ Backend/Utils/sendActivationEmail.ts
@@ -1,11 +1,13 @@
 import { Resend } from 'resend';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async ({to, subject, html}:{to:string, subject:string, html:string}) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'LMS Support <onboarding@resend.dev>', // Use this for testing
+      from: 'LMS Support <support@elearning.com>',
       to: [to],
       subject: subject,
       html: html,
