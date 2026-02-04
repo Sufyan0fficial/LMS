6f263f89042679c5f175e8851fb25e62fa981414
Sufyan0fficial <msufyan0fficial.co@gmail.com>
2026-02-05 02:23:02 +0500

    added custom domain for email sending service

--- Backend/Utils/sendActivationEmail.ts
+++ Backend/Utils/sendActivationEmail.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendMail = async ({to, subject, html}:{to:string, subject:string, html:string}) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'LMS Support <support@elearning.com>',
+      from: 'LMS Support <support@lms.zuporashop.online>',
       to: [to],
       subject: subject,
       html: html,
